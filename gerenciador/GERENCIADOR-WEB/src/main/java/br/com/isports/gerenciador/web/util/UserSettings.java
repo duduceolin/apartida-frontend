dafee7e66c0267133be6212132eb6e1881f7cfa0
Eduardo Ceolin <Eduardo Ceolin>
2016-05-12 03:25:37 -0300

    criação de novos arquivos no projeto web

---
+++ gerenciador/GERENCIADOR-WEB/src/main/java/br/com/isports/gerenciador/web/util/UserSettings.java
@@ -0,0 +1,30 @@
+package br.com.isports.gerenciador.web.util;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class UserSettings implements Serializable {
+
+    private static final long serialVersionUID = 3083775688015310690L;
+
+    private Locale locale;
+
+    @PostConstruct
+    protected void initialize() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+}
