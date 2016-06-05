ec94f6c439e1ba4ddb882bb0761f17626267e3c0
Eduardo Ceolin <Eduardo Ceolin>
2016-06-05 20:33:59 -0300

    Nova operação no serviço de perfil

--- gerenciador/GERENCIADOR-ACESSO/src/main/java/br/com/apartida/acesso/acesso/PerfilServiceAcesso.java
+++ gerenciador/GERENCIADOR-ACESSO/src/main/java/br/com/apartida/acesso/acesso/PerfilServiceAcesso.java
@@ -6,8 +6,10 @@
 package br.com.apartida.acesso.acesso;
 
 import br.com.apartida.acesso.locate.PerfilServiceLocate;
+import br.com.apartida.servicos.perfilservice.InCadastrarAlterarPerfil;
 import br.com.apartida.servicos.perfilservice.InPesquisarPelaEmpresa;
 import br.com.apartida.servicos.perfilservice.IspoException_Exception;
+import br.com.apartida.servicos.perfilservice.OutCadastrarAlterarPerfil;
 import br.com.apartida.servicos.perfilservice.OutPesquisarPerfis;
 import br.com.apartida.servicos.perfilservice.PerfilService;
 import java.io.Serializable;
@@ -32,5 +34,9 @@ public class PerfilServiceAcesso implements Serializable {
 
     public OutPesquisarPerfis pesquisarPerfisPelaEmpresa(InPesquisarPelaEmpresa inPesquisar) throws IspoException_Exception {
         return this.servico.pesquisarPerfisPelaEmpresa(inPesquisar);
+    }
+
+    public OutCadastrarAlterarPerfil cadastrarAlterarPerfil(InCadastrarAlterarPerfil inCadastrar) throws IspoException_Exception {
+        return this.servico.cadastrarAlterarPerfil(inCadastrar);
     }
 }
